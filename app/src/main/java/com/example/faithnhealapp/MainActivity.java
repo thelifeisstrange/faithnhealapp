c15ce388845800797587d11547d2ac025d6d085e
thelifeisstrange <yogeshpk0123@gmail.com>
2024-03-15 01:48:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/faithnhealapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.faithnhealapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private FloatingActionButton floatingActionButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AppointmentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
